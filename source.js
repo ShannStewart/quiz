e1657a6e7b1fab71f93cd08c52329494531088a7
ShannStewart <55711198+ShannStewart@users.noreply.github.com>
2020-01-16 11:43:11 -0500

    Add files via upload

--- source.js
+++ source.js
@@ -1,13 +1,67 @@
-const Store = {
-  questions: [
-    {
-      question: "What is this?",
-      choices: ["A", "B", "C", "D"],
-      answer: "B"
-    },
-      
-  ]
-}
+const STORE = [
+  //00
+  {
+    question: "What is A?",
+    choices: ["A", "B", "C", "D"],
+    answer: "A"
+  },
+  //01  
+  {
+    question: "What is B?",
+    choices: ["A", "B", "C", "D"],
+    answer: "B"
+  },
+  //02
+  {
+    question: "What is C?",
+    choices: ["A", "B", "C", "D"],
+    answer: "C"
+  },
+   //03
+  {
+    question: "What is D?",
+    choices: ["A", "B", "C", "D"],
+    answer: "D"
+  },
+   //04
+  {
+    question: "What is C?",
+    choices: ["A", "B", "C", "D"],
+    answer: "C"
+  },
+   //05
+  {
+    question: "What is B?",
+    choices: ["A", "B", "C", "D"],
+    answer: "B"
+  },
+   //06
+  {
+    question: "What is A?",
+    choices: ["A", "B", "C", "D"],
+    answer: "A"
+  },
+   //07
+  {
+    question: "What is B?",
+    choices: ["A", "B", "C", "D"],
+    answer: "B"
+  },
+   //08
+  {
+    question: "What is C?",
+    choices: ["A", "B", "C", "D"],
+    answer: "C"
+  },
+   //09
+  {
+    question: "What is D?",
+    choices: ["A", "B", "C", "D"],
+    answer: "D"
+  },
+]
 
 let correctAnswers = 0;
 let currentQuestion = 0;
+
+
